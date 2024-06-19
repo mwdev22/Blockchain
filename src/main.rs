d48976d152578572816ef122183b1c81fc5a6070
mwdev22 <maciekwojdyna1@gmail.com>
2024-06-19 11:37:57 +0200

    transaction logic with example added to the main, adding and mining blokcs in loop

--- src/main.rs
+++ src/main.rs
@@ -7,15 +7,15 @@ use blockchain_from_scratch::{ now, Block, BlockChain, BlockHash, Hashable, Tran
 fn main() {
     let difficulty = 0x000ffffffffffffffffffffffffffffff;
     let mut block = Block::new(0, now(), vec![0; 32], 0,vec![Transaction {
-            inputs: vec![ ],
+            inputs: vec![],
             outputs: vec![
                 Transfer {
                     to_addr: "Alice".to_owned(),
-                    value: 50,
+                    value: 505,
                 },
                 Transfer {
                     to_addr: "Bob".to_owned(),
-                    value: 7,
+                    value: 724,
                 },
             ],
         },
@@ -24,49 +24,49 @@ fn main() {
     // displaying the debug fmt implementation
     // block.mine() // bardzo ciężka operacja
 
+    
+    block.mine();
     let mut last_hash = block.hash.clone();
 
-    let mut blockchain = BlockChain {
-        blocks: vec![block],
-        unspent_outputs: HashSet::<BlockHash>::new()
-    };
+    let mut blockchain = BlockChain::new();
+    blockchain.update_with_block(block);
 
     for i in 1..=10 {
-        let mut block = Block::new(i, now(), last_hash , 0, vec![
-        Transaction {
-            inputs: vec![ ],
-            outputs: vec![
-                Transfer {
-                    to_addr: "Chris".to_owned(),
-                    value: 536,
-                },
-            ],
-        },
-        Transaction {
-            inputs: vec![
-                blockchain.blocks[0].transactions[0].outputs[0].clone(),
-            ],
-            outputs: vec![
-                Transfer {
-                    to_addr: "Alice".to_owned(),
-                    value: 360,
-                },
-                Transfer {
-                    to_addr: "Bob".to_owned(),
-                    value: 12,
-                },
-            ],
-        },
-    ], difficulty);
+        let prev_block = &blockchain.blocks[i - 1];
+        let mut block = Block::new(i as u32, now(), last_hash.clone(), 0, vec![
+            Transaction {
+                inputs: vec![],
+                outputs: vec![
+                    Transfer {
+                        to_addr: "Chris".to_owned(),
+                        value: 536 + i as u64,
+                    },
+                ],
+            },
+            Transaction {
+                inputs: vec![
+                    prev_block.transactions[0].outputs[0].clone(),
+                ],
+                outputs: vec![
+                    Transfer {
+                        to_addr: "Alice".to_owned(),
+                        value: 360 + i as u64,
+                    },
+                    Transfer {
+                        to_addr: "Bob".to_owned(),
+                        value: 12 + i as u64,
+                    },
+                ],
+            },
+        ], difficulty);
+        
         // displaying the debug fmt implementation
         println!("{:?}", &block);
-        let h = block.hash();
         block.mine();
-        println!("mined: {:?} hash", &block);
+        println!("mined: {:?} hash", &block.hash());
         last_hash = block.hash.clone();
         blockchain.update_with_block(block).expect("Failed to add block");
     }
     
-    // TODO transactions logic
 
 }
