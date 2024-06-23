fef9e12d2f7dde038b22e2d2024447543b65fd01
mwdev22 <maciekwojdyna1@gmail.com>
2024-06-23 19:45:21 +0200

    wallets implemented

--- src/blockchain/wallet.rs
+++ src/blockchain/wallet.rs
@@ -1,18 +1,62 @@
-use crate::*;
+use std::fmt::Debug;
 
-#[derive(Debug, Clone)]
+use crate::*;
+use rsa::{pkcs1::{EncodeRsaPrivateKey, EncodeRsaPublicKey},  RsaPrivateKey, RsaPublicKey};
+use rand::rngs::OsRng;
+use sha2::{Sha256, Digest};
+
+
+#[derive(Clone)]
 pub struct Wallet {
-    private_key: String,
-    public_key: String,
+    username: String,
+    private_key: RsaPrivateKey,
+    public_key: RsaPublicKey,
     balance: f64
-} 
+}
+
+impl Wallet {
+    pub fn new(username: String) -> Self {
+        let (private_key, public_key) = Self::generate_keys();
+        Wallet {
+            username,
+            private_key,
+            public_key,
+            balance: 0.0,
+        }
+    }
+    fn generate_keys() -> (RsaPrivateKey, RsaPublicKey) {
+        let mut rng = OsRng;
+        let bits = 2048;
+        let private_key = RsaPrivateKey::new(&mut rng, bits)
+            .expect("failed to generate a key");
+        let public_key = RsaPublicKey::from(&private_key);
+        (private_key, public_key)
+    }
+
+    pub fn reset_keys(&mut self) {
+        let (private_key, public_key) = Self::generate_keys();
+        self.private_key = private_key;
+        self.public_key = public_key;
+    }
+
+    pub fn set_balance(&mut self, balance: f64) {
+        self.balance = balance;
+    }
+
+}
 
 impl Hashable for Wallet {
     fn bytes(&self) -> Vec<u8> {
-        let mut bytes = vec![];
-        bytes.extend(self.private_key.as_bytes());
-        bytes.extend(self.public_key.as_bytes());
-        bytes.extend(self.balance.to_be_bytes());
-        bytes
+        let mut hasher = Sha256::new();
+        hasher.update(self.private_key.to_pkcs1_der().unwrap().as_bytes());
+        hasher.update(self.public_key.to_pkcs1_der().unwrap().as_bytes());
+        hasher.update(&self.balance.to_be_bytes());
+        hasher.finalize().to_vec()
+    }
+}
+
+impl Debug for Wallet {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        write!(f,"{}'s wallet with balance {}", &self.username, &self.balance)
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,47 +1,51 @@
+
 #![allow(unused)]
-use std::collections::HashSet;
 use rand::Rng;
-use blockchain_from_scratch::{ now, Block, BlockChain, BlockHash, Hashable, Transfer, Transaction};
+use blockchain_from_scratch::{now, Block, BlockChain, BlockHash, Hashable, Transfer, Transaction, Wallet};
 
 
 fn main() {
     let difficulty = 0x000ffffffffffffffffffffffffffffff;
-    let mut block = Block::new(0, now(), vec![0; 32], 0,vec![Transaction {
-            inputs: vec![],
-            outputs: vec![
-                Transfer {
-                    to_addr: "Alice".to_owned(),
-                    value: 505,
-                },
-                Transfer {
-                    to_addr: "Bob".to_owned(),
-                    value: 724,
-                },
-            ],
-        },
-    
-    ], difficulty);
-    // displaying the debug fmt implementation
-    // block.mine() // bardzo ciężka operacja
 
-    
+    // create wallets
+    let mut maciek_wallet = Wallet::new("Alice".to_string());
+    let mut john_wallet = Wallet::new("Bob".to_string());
+    let mut chris_wallet = Wallet::new("Chris".to_string());
+
+
+    maciek_wallet.set_balance(505.0);
+    john_wallet.set_balance(724.0);
+
+    let mut block = Block::new(0, now(), vec![0; 32], 0, vec![Transaction {
+        inputs: vec![],
+        outputs: vec![
+            Transfer {
+                receiver: maciek_wallet.clone(),
+                value: 505,
+            },
+            Transfer {
+                receiver: john_wallet.clone(),
+                value: 724,
+            },
+        ],
+    }], difficulty);
+
     block.mine();
     let mut last_hash = block.hash.clone();
 
     let mut blockchain = BlockChain::new();
-    blockchain.update_with_block(block);
+    blockchain.update_with_block(block).expect("error adding first block");
 
     let mut rng = rand::thread_rng();
 
-
     for i in 1..=10 {
         let prev_block = &blockchain.blocks[i - 1];
-        let mut transactions = vec![
+        let transactions = vec![
             Transaction {
                 inputs: vec![],
                 outputs: vec![
                     Transfer {
-                        to_addr: "Chris".to_owned(),
+                        receiver: chris_wallet.clone(),
                         value: 500 + rng.gen_range(0..100), // random value
                     },
                 ],
@@ -52,11 +56,11 @@ fn main() {
                 ],
                 outputs: vec![
                     Transfer {
-                        to_addr: "Alice".to_owned(),
+                        receiver: maciek_wallet.clone(),
                         value: 300 + rng.gen_range(0..100), // random value
                     },
                     Transfer {
-                        to_addr: "Bob".to_owned(),
+                        receiver: john_wallet.clone(),
                         value: 10 + rng.gen_range(0..10), // random value
                     },
                 ],
@@ -64,7 +68,6 @@ fn main() {
         ];
 
         let mut block = Block::new(i as u32, now(), last_hash.clone(), 0, transactions, difficulty);
-        // displaying the debug fmt implementation
         println!("{:?}", &block);
         block.mine();
         println!("mined: {:?} hash", &block.hash());
@@ -72,6 +75,4 @@ fn main() {
         blockchain.update_with_block(block).expect("Failed to add block");
     }
     println!("Total unspent amount: {}", blockchain.total_unspent());
-    
-
 }
